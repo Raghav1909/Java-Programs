820382434c1e4fd95df4f719a278e2c54fbcb3d6
Pranav K Bhaskar <pkb19032002@gmail.com>
2021-12-27 21:03:01 +0530

    Changes made

---
+++ MethodOverriding.java
@@ -0,0 +1,26 @@
+class X {
+    void display() {
+        System.out.println("This is class X");
+    }
+}
+
+class Y extends X {
+    void display() {
+        System.out.println("This is class Y");
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String args[]) {
+        X x1 = new X();
+        x1.display();
+
+        Y y1 = new Y();
+        y1.display();
+
+        X refX = new X();
+        refX.display();
+        refX = new Y();
+        refX.display();
+    }
+}
